300f56eb0bf3bb92dc05f87340b046701d8e9f91
0326 <winniringy@gmail.com>
2025-10-19 01:22:08 +0800

    feat: 支持tab模式

--- src/pages/popup/Content.tsx
+++ src/pages/popup/Content.tsx
@@ -1,23 +1,28 @@
-import React, { useState, useMemo } from 'react';
-import Modal from '../../components/Modal';
-import { getFaviconURL, updateBookmark, removeBookmark, i18n } from '../../services'
-import TextInput from '../../components/TextInput';
+import React, { useState, useMemo } from "react";
+import Modal from "../../components/Modal";
+import {
+  getFaviconURL,
+  updateBookmark,
+  removeBookmark,
+  i18n,
+} from "../../services";
+import TextInput from "../../components/TextInput";
 
 interface ContentProps {
-    dataSource: chrome.bookmarks.BookmarkTreeNode;
-    refresh: () => void;
+  dataSource: chrome.bookmarks.BookmarkTreeNode;
+  refresh: () => void;
 }
 interface BookmarkNode {
-    id: string;
-    title: string;
-    url?: string;
-    path: string;
-    icon: string;
+  id: string;
+  title: string;
+  url?: string;
+  path: string;
+  icon: string;
 }
 interface BookmarkNodes {
-    id: string;
-    title: string;
-    list: BookmarkNode[];
+  id: string;
+  title: string;
+  list: BookmarkNode[];
 }
 /**
  * 将多层文件夹flat为一层，当前书签
@@ -25,148 +30,247 @@ interface BookmarkNodes {
  * @returns A flat array of bookmark nodes.
  */
 const flatBookmarks = (data: chrome.bookmarks.BookmarkTreeNode) => {
-    if (!data?.children?.length) return [];
-    const result: BookmarkNodes[] = [];
-    const recurse = (nodes: chrome.bookmarks.BookmarkTreeNode[], path: string, result: BookmarkNodes) => {
-        for (const node of nodes) {
-            if (node.children) {
-                recurse(node.children, `${path}/${node.title}`, result);
-            } else {
-                result.list.push({
-                    id: node.id,
-                    title: node.title,
-                    url: node.url,
-                    icon: getFaviconURL(node.url || ''),
-                    path,
-                });
-            }
-        }
-    };
-
-    const directNodes: BookmarkNodes = {
-        id: '-1',
-        title: data.title,
-        list: [],
-    };
-    data.children.forEach(item => {
-        const path = `${data.title}/${item.title}`;
-        if (item.children) {
-            const folder: BookmarkNodes = {
-                id: item.id,
-                title: item.title,
-                list: [],
-            };
-            recurse(item.children, path, folder);
-            result.push(folder);
-        } else {
-            directNodes.list.push({
-                id: item.id,
-                title: item.title,
-                url: item.url,
-                icon: getFaviconURL(item.url || ''),
-                path,
-            });
-        }
-    });
-    if (directNodes.list.length > 0) {
-        result.push(directNodes);
+  if (!data?.children?.length) return [];
+  const result: BookmarkNodes[] = [];
+  const recurse = (
+    nodes: chrome.bookmarks.BookmarkTreeNode[],
+    path: string,
+    result: BookmarkNodes
+  ) => {
+    for (const node of nodes) {
+      if (node.children) {
+        recurse(node.children, `${path}/${node.title}`, result);
+      } else {
+        result.list.push({
+          id: node.id,
+          title: node.title,
+          url: node.url,
+          icon: getFaviconURL(node.url || ""),
+          path,
+        });
+      }
     }
-    console.log('flatBookmarks result', result);
-    return result;
+  };
+
+  const directNodes: BookmarkNodes = {
+    id: "-1",
+    title: data.title,
+    list: [],
+  };
+  data.children.forEach((item) => {
+    const path = `${data.title}/${item.title}`;
+    if (item.children) {
+      const folder: BookmarkNodes = {
+        id: item.id,
+        title: item.title,
+        list: [],
+      };
+      recurse(item.children, path, folder);
+      result.push(folder);
+    } else {
+      directNodes.list.push({
+        id: item.id,
+        title: item.title,
+        url: item.url,
+        icon: getFaviconURL(item.url || ""),
+        path,
+      });
+    }
+  });
+  if (directNodes.list.length > 0) {
+    result.push(directNodes);
+  }
+  console.log("flatBookmarks result", result);
+  return result;
 };
 
 const Content: React.FC<ContentProps> = ({ dataSource, refresh }) => {
-    const [showModal, setShowModal] = useState(false);
-    const [selectedBookmark, setSelectedBookmark] = useState<BookmarkNode | null>(null);
-    const list = useMemo(() => flatBookmarks(dataSource), [dataSource]);
-    const onBookmarkClick = (bookmark: BookmarkNode) => {
-        setSelectedBookmark(bookmark);
-        setShowModal(true);
+  const [showModal, setShowModal] = useState(false);
+  const [current, setCurrent] = useState(0);
+  const [selectedBookmark, setSelectedBookmark] = useState<BookmarkNode | null>(
+    null
+  );
+  const list = useMemo(() => flatBookmarks(dataSource), [dataSource]);
+  const onBookmarkClick = (bookmark: BookmarkNode) => {
+    setSelectedBookmark(bookmark);
+    setShowModal(true);
+  };
+
+  const handleModalOk = async () => {
+    if (selectedBookmark?.title && selectedBookmark?.url) {
+      await updateBookmark(selectedBookmark.id, {
+        title: selectedBookmark.title,
+        url: selectedBookmark.url,
+      });
+      setShowModal(false);
+      refresh();
     }
+  };
 
-    const handleModalOk = async () => {
-        if (selectedBookmark?.title && selectedBookmark?.url) {
-            await updateBookmark(selectedBookmark.id, { title: selectedBookmark.title, url: selectedBookmark.url });
-            setShowModal(false);
-            refresh();
-        }
-    };
+  const handleModalDelete = async () => {
+    if (selectedBookmark) {
+      await removeBookmark(selectedBookmark.id);
+      setShowModal(false);
+      refresh();
+    }
+  };
 
-    const handleModalDelete = async () => {
-        if (selectedBookmark) {
-            await removeBookmark(selectedBookmark.id);
-            setShowModal(false);
-            refresh();
-        }
-    };
+  const renderListMode = () =>
+    list?.map((item) => (
+      <section className="w-full divide-y rounded divide-slate-200 ">
+        <details
+          className="p-4 group border-b-1 border-dashed border-emerald-200"
+          open
+        >
+          <summary className="relative cursor-pointer list-none pr-8 font-medium text-slate-700 transition-colors duration-300 focus-visible:outline-none group-hover:text-slate-900  [&::-webkit-details-marker]:hidden">
+            📚{" "}
+            {dataSource.title === item.title
+              ? `${dataSource.title}`
+              : `${dataSource.title} / ${item.title}`}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute right-0 w-4 h-4 transition duration-300 top-1 shrink-0 stroke-slate-700 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              aria-labelledby="title-ac01 desc-ac01"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          {/* <div className="mt-4 text-slate-500 grid grid-cols-4 gap-3 md:grid-cols-5 lg:grid-cols-6"> */}
+          <div className="mt-4 text-slate-500 grid grid-cols-4 gap-3 md:grid-cols-5">
+            {item.list.map((bookmark) => (
+              <div key={bookmark.id} className="truncate flex items-center">
+                <img
+                  src={bookmark.icon}
+                  onClick={() => onBookmarkClick(bookmark)}
+                  alt=""
+                  className="inline-block w-4 h-4 mr-2 cursor-pointer"
+                />
+                <a
+                  href={bookmark.url}
+                  target="_blank"
+                  className="hover:underline"
+                >
+                  {bookmark.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </details>
+      </section>
+    ));
 
+  const renderTabMode = () => {
     return (
-        <>
-        <Modal
-            visible={showModal}
-            title={i18n("editBookmark")}
-            content={(
-                <div>
-                    <TextInput
-                        name={i18n("name")}
-                        placeholder={i18n("namePlaceholder")}
-                        value={selectedBookmark?.title}
-                        // @ts-ignore
-                        onChange={(value) => setSelectedBookmark({ ...selectedBookmark, title: value })}
+      <section className="m-4" aria-multiselectable="false">
+        <ul
+          className="flex items-center gap-2 overflow-x-scroll scrollbar-hide"
+          role="tablist"
+        >
+          {list?.map((item, index) => (
+            <li role="presentation">
+              <button
+                className={`inline-flex h-8 items-center justify-center gap-2 whitespace-nowrap rounded px-4 text-xs font-medium tracking-wide transition duration-300 focus-visible:outline-none disabled:cursor-not-allowed ${
+                  current === index
+                    ? "bg-emerald-500 text-white hover:bg-emerald-600 focus:bg-emerald-700 disabled:bg-emerald-300"
+                    : "w-full justify-self-center stroke-slate-700 text-slate-700 hover:bg-emerald-50 hover:stroke-emerald-500 hover:text-emerald-500 focus:bg-emerald-50 focus:stroke-emerald-600 focus:text-emerald-600 disabled:text-emerald-300"
+                }`}
+                id={`tab-label-${index}f`}
+                role="tab"
+                aria-setsize="3"
+                aria-posinset="1"
+                tabindex={current === index ? 0 : -1}
+                aria-controls={`tab-panel-${index}f`}
+                aria-selected="true"
+                onClick={() => {
+                  setCurrent(index);
+                }}
+              >
+                <span>{item.title}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div>
+          {list?.map((item, index) => (
+            <div
+              className={`px-4 pb-4 text-sm ${current === index ? "" : "hidden"}`}
+              id="tab-panel-1f"
+              aria-hidden={`${current === index ? "true" : "false"}`}
+              role="tabpanel"
+              aria-labelledby="tab-label-1f"
+              tabIndex={-1}
+            >
+              <div className="mt-4 text-slate-500 grid grid-cols-4 gap-3 md:grid-cols-3">
+                {item.list.map((bookmark) => (
+                  <div key={bookmark.id} className="truncate flex items-center">
+                    <img
+                      src={bookmark.icon}
+                      onClick={() => onBookmarkClick(bookmark)}
+                      alt=""
+                      className="inline-block w-4 h-4 mr-2 cursor-pointer"
                     />
-                    <TextInput
-                        name={i18n("url")}
-                        placeholder={i18n("urlPlaceholder")}
-                        value={selectedBookmark?.url}
-                        // @ts-ignore
-                        onChange={(value) => setSelectedBookmark({ ...selectedBookmark, url: value })}
-                    />
-                </div>
-            )}
-            okText={i18n("confirm")}
-            cancelText={i18n("cancel")}
-            delText={i18n("delete")}
-            onOk={handleModalOk}
-            onDelete={handleModalDelete}
-            onClose={() => setShowModal(false)}
-        />
-        {list?.map((item) => (
-            <section className="w-full divide-y rounded divide-slate-200 ">
-                <details className="p-4 group border-b-1 border-dashed border-emerald-200" open>
-                    <summary className="relative cursor-pointer list-none pr-8 font-medium text-slate-700 transition-colors duration-300 focus-visible:outline-none group-hover:text-slate-900  [&::-webkit-details-marker]:hidden">
-                        📚 {dataSource.title === item.title ? `${dataSource.title}` : `${dataSource.title} / ${item.title}`}
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="absolute right-0 w-4 h-4 transition duration-300 top-1 shrink-0 stroke-slate-700 group-open:rotate-45"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        aria-labelledby="title-ac01 desc-ac01"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 4v16m8-8H4"
-                            />
-                        </svg>
-                    </summary>
-                    {/* <div className="mt-4 text-slate-500 grid grid-cols-4 gap-3 md:grid-cols-5 lg:grid-cols-6"> */}
-                    <div className="mt-4 text-slate-500 grid grid-cols-4 gap-3 md:grid-cols-5">
-                        {item.list.map((bookmark) => (
-                            <div key={bookmark.id} className="truncate flex items-center">
-                                <img src={bookmark.icon} onClick={() => onBookmarkClick(bookmark)} alt="" className="inline-block w-4 h-4 mr-2 cursor-pointer" />
-                                <a href={bookmark.url} target='_blank' className="hover:underline">
-                                    {bookmark.title}
-                                </a>
-                            </div>
-                        ))}
-                    </div>
-                </details>
-            </section>
-        ))}
-        </>
+                    <a
+                      href={bookmark.url}
+                      target="_blank"
+                      className="hover:underline"
+                    >
+                      {bookmark.title}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
+  };
+
+  return (
+    <>
+      <Modal
+        visible={showModal}
+        title={i18n("editBookmark")}
+        content={
+          <div>
+            <TextInput
+              name={i18n("name")}
+              placeholder={i18n("namePlaceholder")}
+              value={selectedBookmark?.title}
+              // @ts-ignore
+              onChange={(value) =>
+                setSelectedBookmark({ ...selectedBookmark, title: value })
+              }
+            />
+            <TextInput
+              name={i18n("url")}
+              placeholder={i18n("urlPlaceholder")}
+              value={selectedBookmark?.url}
+              // @ts-ignore
+              onChange={(value) =>
+                setSelectedBookmark({ ...selectedBookmark, url: value })
+              }
+            />
+          </div>
+        }
+        okText={i18n("confirm")}
+        cancelText={i18n("cancel")}
+        delText={i18n("delete")}
+        onOk={handleModalOk}
+        onDelete={handleModalDelete}
+        onClose={() => setShowModal(false)}
+      />
+      {list?.length > 5 ? renderTabMode() : renderListMode()}
+    </>
+  );
 };
 
 export default Content;
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,38 +2,41 @@
  * Fetches the user's bookmarks from the Chrome bookmarks API.
  */
 export async function getBookmarks() {
-    const data = await chrome.bookmarks.getTree()
-    // console.log('getBookmarks data', data)
-    return data[0].children;
+  const data = await chrome.bookmarks.getTree();
+  // console.log('getBookmarks data', data)
+  return data[0].children;
 }
 
 export async function searchBookmarks(query: string) {
-    const bookmarks = await chrome.bookmarks.search(query);
-    return bookmarks;
+  const bookmarks = await chrome.bookmarks.search(query);
+  return bookmarks;
 }
 
-export async function updateBookmark(id: string, changes: { title?: string; url?: string }) {
-    const rs = await chrome.bookmarks.update(id, changes);
-    return rs;
+export async function updateBookmark(
+  id: string,
+  changes: { title?: string; url?: string }
+) {
+  const rs = await chrome.bookmarks.update(id, changes);
+  return rs;
 }
 
 export async function removeBookmark(bookmarkId: string) {
-    const rs = await chrome.bookmarks.remove(bookmarkId);
-    return rs;
+  const rs = await chrome.bookmarks.remove(bookmarkId);
+  return rs;
 }
 
 export function i18n(key: string) {
-    const rs = chrome.i18n.getMessage(key);
-    console.log('i18n', { key, rs });
-    return rs;
+  const rs = chrome.i18n.getMessage(key);
+  console.log("i18n", { key, rs });
+  return rs;
 }
 
 export function getFaviconURL(u: string) {
-    const url = new URL(chrome.runtime.getURL("/_favicon/"));
-    url.searchParams.set("pageUrl", u || 'https://developer.chrome.com');
-    url.searchParams.set("size", "32");
-    // console.log('getFaviconURL', url.toString())
-    return url.toString();
+  const url = new URL(chrome.runtime.getURL("/_favicon/"));
+  url.searchParams.set("pageUrl", u || "https://developer.chrome.com");
+  url.searchParams.set("size", "32");
+  // console.log('getFaviconURL', url.toString())
+  return url.toString();
 }
 
 // export async function setFullWindow() {
